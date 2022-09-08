d6678d35cfe285acaff988d3bf3a0185f627b1fb
evgeny3322 <retard_x_98@mail.ru>
2022-09-08 10:47:47 +0300

    Updates

---
+++ precache-manifest.ccaa4a4d93d00b750d5fbc6badd13411.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e4e7f5b561698f72aa3",
+    "url": "/evgeny3322/kanalservis-test/static/css/main.b02d6b7f.chunk.css"
+  },
+  {
+    "revision": "5e4e7f5b561698f72aa3",
+    "url": "/evgeny3322/kanalservis-test/static/js/main.56012a17.chunk.js"
+  },
+  {
+    "revision": "bec944c8220e541fabe0",
+    "url": "/evgeny3322/kanalservis-test/static/js/runtime~main.ff05c824.js"
+  },
+  {
+    "revision": "df773a2c984b9c2af264",
+    "url": "/evgeny3322/kanalservis-test/static/js/2.e72e9d17.chunk.js"
+  },
+  {
+    "revision": "a16ae9df81c80da6726c",
+    "url": "/evgeny3322/kanalservis-test/static/js/3.4254923f.chunk.js"
+  },
+  {
+    "revision": "b45015d0dfa0378e24c1794737bbf67f",
+    "url": "/evgeny3322/kanalservis-test/static/media/logo.b45015d0.svg"
+  },
+  {
+    "revision": "d4074ea48a8758840a8f215ebea33de7",
+    "url": "/evgeny3322/kanalservis-test/static/media/logo_short.d4074ea4.svg"
+  },
+  {
+    "revision": "8df68d8577560a4ad5be459524d53f46",
+    "url": "/evgeny3322/kanalservis-test/static/media/exit.8df68d85.svg"
+  },
+  {
+    "revision": "cd1c63ff04b5ed2d9c15fdfd16f64141",
+    "url": "/evgeny3322/kanalservis-test/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/evgeny3322/kanalservis-test/precache-manifest.ccaa4a4d93d00b750d5fbc6badd13411.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/evgeny3322/kanalservis-test/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
