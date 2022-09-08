28e3cf6be6c1c8211899c2c4f68173633258bcf2
evgeny3322 <retard_x_98@mail.ru>
2022-09-08 10:37:30 +0300

    gh-pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthPage from "../components/Pages/AuthPage/AuthPage";
 import NotFound from "../components/Pages/NotFound/NotFound";
